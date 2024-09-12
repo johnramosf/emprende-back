fb772accb1d2609bb4e01df2fc0fce8325dae066
John Ramos <johnalex.ramosf@gmail.com>
2024-09-12 11:12:26 -0500

    Listo para ir a github

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http")
 
 function requestController() {
     // logíca de nuesta aplicación
-    console.log({message:"Hola mundo"})
+    console.log({message:"Hola mundo!!!"})
 }
 
 //configurar nuestro server
